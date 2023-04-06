6b080f098c0826276d8be04d7cc500a691962981
xincici <llxy8687@foxmail.com>
2023-04-06 20:04:00 +0800

    feat: save work

--- src/components/GuessNumber.vue
+++ src/components/GuessNumber.vue
@@ -11,11 +11,37 @@
     </h2>
     <HelpDialog :help-show="helpShow" @hideHelp="helpShow = false" />
     <audio :src="audio" ref="audioRef" loop="true"></audio>
+    <button @click="initGame" class="game-icon">{{ i18n('start') }}</button>
+    <div class="game-area">
+      <p
+        v-for="(oneGuess, idx) in guessHistory"
+        :key="idx"
+        class="answer-line"
+        >
+        <span
+          v-for="(number, index) in oneGuess"
+          :key="index"
+          class="number-card"
+          >{{ number }}</span>
+      </p>
+      <p>
+        <input
+          class="number-input"
+          v-model="currentGuess" />
+      </p>
+      <p>
+        <button
+          :disabled="enterDisabled"
+          @click="guessOnce"
+        >Enter</button>
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from 'vue';
+import { ref, watch, computed, onMounted } from 'vue';
+import sampleSize from 'lodash.samplesize';
 import Toggle from '@vueform/toggle';
 import '@vueform/toggle/themes/default.css';
 import audio from '../assets/yzcw.mp3';
@@ -23,14 +49,11 @@ import HelpDialog from './HelpDialog.vue';
 import { language } from '../plugins/i18n';
 import { theme } from '../utils/theme';
 
-const audioPlay = ref(false);
-const helpShow = ref(false);
-const audioRef = ref(null);
 
 const metaThemeColorEl = document.querySelector('meta[name="theme-color"]');
-
 let lastColor = metaThemeColorEl.getAttribute('content');
 
+const helpShow = ref(false);
 watch(helpShow, val => {
   if (val) {
     lastColor = metaThemeColorEl.getAttribute('content');
@@ -39,12 +62,39 @@ watch(helpShow, val => {
     metaThemeColorEl.setAttribute('content', lastColor);
   }
 });
+
+const audioRef = ref(null);
+const audioPlay = ref(false);
 onMounted(() => {
   watch(audioPlay, val => {
     if (val === 'play') audioRef.value.play();
     else audioRef.value.pause();
   });
 });
+
+const anwser = ref([]);
+const gameSize = ref(4);
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+function initGame() {
+  anwser.value = sampleSize(numbers, gameSize.value);
+  console.log(anwser.value)
+}
+onMounted(() => {
+  initGame();
+});
+
+const guessHistory = ref([]);
+const currentGuess = ref('');
+const enterDisabled = computed(() => {
+  if (currentGuess.value.length === gameSize.value) return false;
+  return true;
+});
+function guessOnce() {
+  console.log('hhh');
+  guessHistory.value.push(currentGuess.value.split('').map(v => +v));
+  currentGuess.value = '';
+  console.log(guessHistory.value);
+}
 </script>
 
 <style scoped lang="scss">
@@ -82,6 +132,20 @@ onMounted(() => {
     font-size: 20px;
     color: #c33;
     margin-left: 10px;
+  }
+  .game-area {
+    .answer-line {
+      margin: 5px 0;
+      .number-card {
+        display: inline-block;
+        padding: 10px 15px;
+        font-size: 18px;
+        font-weight: bold;
+        color: #ccc;
+        background: rgba(0, 0, 0, 0.5);
+        margin: 3px;
+      }
+    }
   }
 }
 @media only screen and (min-width: 320px) and (max-width: 720px) {
