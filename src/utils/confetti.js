9bc4fd1ac423377756f1fe986077ff36451edb13
xincici <llxy8687@foxmail.com>
2023-05-06 20:07:22 +0800

    feat: modify confetti again

--- src/utils/confetti.js
+++ src/utils/confetti.js
@@ -18,22 +18,22 @@ export default function() {
   const end = Date.now() + 1200;
   (function frame() {
     confetti({
-      particleCount: 2,
+      colors,
+      particleCount: 5,
       angle: 60,
       spread: 55,
       origin: { x: 0 },
-      colors: colors
     });
     confetti({
-      particleCount: 2,
+      colors,
+      particleCount: 5,
       angle: 120,
       spread: 55,
       origin: { x: 1 },
-      colors: colors
     });
   
     if (Date.now() < end) {
-      requestAnimationFrame(frame);
+      setTimeout(frame, 40);
     }
   }());
 }
